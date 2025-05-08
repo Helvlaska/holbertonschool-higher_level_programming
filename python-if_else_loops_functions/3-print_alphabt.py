7e1a1abd139a2885442340383f3080d8f09e917b
Helvlaska <ccastan@live.fr>
2025-05-08 12:50:58 +0200

    :sparkles: (3-print_alphabt.py) : Exclude 'e' and 'q' from alphabet using single loop and .format() print

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+alphabet = ""
+
+for i in range(97, 123):
+    if i == 101 or i == 113:
+        continue
+    else:
+        alphabet += chr(i)
+
+print("{}".format(alphabet), end="")
