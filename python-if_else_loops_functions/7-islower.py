ff3eef4ab3332f48dead65d6349807c5634fbe83
Helvlaska <ccastan@live.fr>
2025-05-09 19:07:06 +0200

    :sparkles: (8-uppercase.py) : Implement function to convert lowercase letters to uppercase

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,13 @@
-#!/usr/bin/python3
-
 def islower(c):
-    return ord('a') <= ord(c) <= ord('z')
+    # on récupère la valeur ASCII du caractère c
+    ascii_value = ord(c)
+
+    # on définit les bornes pour les lettres minuscules
+    min_lower = ord('a')
+    max_lower = ord('z')
+
+    # on vérifie si la valeur ASCII du caractère c est dans l'intervalle des minuscules
+    if ascii_value >= min_lower and ascii_value <= max_lower:
+        return True  # si oui, c'est une lettre minuscule
+    else:
+        return False  # sinon, ce n'est pas une lettre minuscule
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    # Créer une variable vide pour accumuler les résultats modifiés
+    upper_string = ""
+
+    # Boucle sur chaque caractère de la chaîne d'entrée
+    for char in str:
+        # On récupère la valeur ASCII du caractère actuel
+        ascii_value = ord(char)
+
+        # Vérification si le caractère est une lettre minuscule
+        if ascii_value >= ord('a') and ascii_value <= ord('z'):
+            # Si c'est une lettre minuscule, on la convertit en majuscule
+            # En soustrayant 32 de la valeur ASCII
+            new_char = chr(ascii_value - 32)
+
+            # Ajouter le caractère modifié (majuscule) dans la chaîne de résultat
+            upper_string += new_char
+        else:
+            # Si ce n'est pas une lettre minuscule, on garde le caractère tel quel
+            upper_string += char
+
+    # Imprimer le résultat final sans retour à la ligne supplémentaire
+    print(upper_string)
