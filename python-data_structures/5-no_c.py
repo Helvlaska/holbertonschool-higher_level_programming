309d459052cefcae90201c86f9c5829f3daca8ba
Helvlaska <ccastan@live.fr>
2025-05-12 15:39:10 +0200

    :hammer: (5-no_c.py) : Clean up whitespace in no_c function for improved readability

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    
+
     string = ""
-    
+
     for i in my_string:
         valeur_ascii = ord(i)
         if valeur_ascii == 99 or valeur_ascii == 67:
