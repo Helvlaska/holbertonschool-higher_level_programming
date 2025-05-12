3bd34aa8c923645aaa186808fbfb66a3f6f8a8c2
Helvlaska <ccastan@live.fr>
2025-05-12 14:02:24 +0200

    :hammer: (4-new_in_list.py) : Refactor list copying method for clarity

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    b = my_list[:] #copie de la liste originale
+    b = list(my_list) #copie de la liste originale
     if idx < 0 or idx >= len(b):
         return b
     else:
