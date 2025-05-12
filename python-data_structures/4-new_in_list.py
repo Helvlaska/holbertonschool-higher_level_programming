b9b11c49ae5cfce36a029dd9e3a655121d3258b5
Helvlaska <ccastan@live.fr>
2025-05-12 14:18:26 +0200

    :hammer: (4-new_in_list.py) : Correct return value for out-of-bounds index in new_in_list function

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     b = my_list.copy() #copie de la liste originale
     if idx < 0 or idx >= len(b):
-        return b
+        return my_list
     else:
         b[idx] = element
         return b
