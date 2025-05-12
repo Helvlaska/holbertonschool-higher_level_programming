2b850397d1178cbb03b2baf6bef543281fdfe6c3
Helvlaska <ccastan@live.fr>
2025-05-12 14:25:28 +0200

    :hammer: (4-new_in_list.py) : Refactor new_in_list function to improve readability by removing unnecessary comments and maintaining structure (pycodestyle)

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    b = my_list.copy() #copie de la liste originale
+    b = my_list.copy()
     if idx < 0 or idx >= len(b):
         return my_list
-    
-    b[idx] = element
-    return b
+    else:
+        b[idx] = element
+        return b
