1225472dcd1c3ea0d470185095527b923bcfc66c
Helvlaska <ccastan@live.fr>
2025-05-12 14:21:14 +0200

    :hammer: (4-new_in_list.py) : Refactor new_in_list function to remove unnecessary else statement for clarity

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -4,6 +4,6 @@ def new_in_list(my_list, idx, element):
     b = my_list.copy() #copie de la liste originale
     if idx < 0 or idx >= len(b):
         return my_list
-    else:
-        b[idx] = element
-        return b
+    
+    b[idx] = element
+    return b
