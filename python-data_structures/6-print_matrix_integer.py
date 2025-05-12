aac4fa9b5f0c6d8ce68a495b2ad11a18326935a1
Helvlaska <ccastan@live.fr>
2025-05-12 17:39:15 +0200

    :hammer: (6-print_matrix_integer.py) : Refactor print_matrix_integer function to improve performance and readability by eliminating string concatenation

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    ligne = ""
-    for i in matrix:
-        for j in i:
-            ligne += "{} ".format(j)
-        ligne += "\n"
-    print("{}".format(ligne), end="")
+    for ligne in matrix:
+        i = 0
+        while i < len(ligne):
+            print("{:d}".format(ligne[i]), end="")
+            if i != len(ligne) - 1:
+                print(" ", end="")
+            i += 1
+        print()
+
