0f95b1864bbe260254008922fa67d439182c5d45
Helvlaska <ccastan@live.fr>
2025-05-14 10:44:43 +0200

    :hammer: (6-print_sorted_dictionary.py) : formatting in function

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 
 def print_sorted_dictionary(a_dictionary):
     for order_key in sorted(a_dictionary):
-        print(f"{order_key} : {a_dictionary[order_key]}")
+        print(f"{order_key}: {a_dictionary[order_key]}")
