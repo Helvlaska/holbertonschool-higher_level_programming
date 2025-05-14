7e93d7ebc9ef3f54b6864210bceacac11c9523bb
Helvlaska <ccastan@live.fr>
2025-05-15 00:07:53 +0200

    :sparkles: (12-roman_to_int.py) : Add function to convert Roman numerals to integers

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    result = 0
+    dict_trad = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    for i in range(len(roman_string)):
+        char = roman_string[i]
+        pr_char = roman_string[i - 1]
+        if char in dict_trad:
+            if i > 0 and pr_char == 'I' and (char == 'V' or char == 'X'):
+                twist = dict_trad[char] - 2
+                result = result + twist
+            else:
+                result = result + dict_trad[char]
+        else:
+            return 0
+    return result
