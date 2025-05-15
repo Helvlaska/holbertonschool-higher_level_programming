d37f8bd5f4ad27009b2fc5e6e37997d2338e6ce0
Helvlaska <ccastan@live.fr>
2025-05-15 14:11:44 +0200

    :hammer: (0-safe_print_list.py) : Remove extra space in print statement for consistency

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for _ in range(x):
         try:
-            print(my_list[count], end= "")
+            print(my_list[count], end="")
             count = count + 1
         except IndexError:
             break
