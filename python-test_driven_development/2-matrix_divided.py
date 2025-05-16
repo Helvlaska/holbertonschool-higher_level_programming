af4d98bb0495ee886ca742812a46c45501f32ed1
Helvlaska <ccastan@live.fr>
2025-05-16 20:37:31 +0200

    :hammer: (2-matrix_divided.py) : Refactor error messages in matrix_divided function for improved readabilitychecks

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -9,17 +9,24 @@ def matrix_divided(matrix, div):
     """Divides all elements of a matrix by div, rounded to 2 decimals."""
 
     if not isinstance(matrix, list) or matrix == []:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     if not all(isinstance(el, (int, float)) for row in matrix for el in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
 
     row_len = len(matrix[0])
     if not all(len(row) == row_len for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
+        raise TypeError(
+            "Each row of the matrix must have the same size"
+        )
 
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
