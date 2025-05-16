10e7307c2489465868b9f14e75a66c71bbf60f26
Helvlaska <ccastan@live.fr>
2025-05-16 15:35:23 +0200

    📝   (0-add_integer.py) : Update module docstring and function docstring for clarity and completeness

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""
+This module provides a function to safely add two integers.
+The inputs must be integers or floats, otherwise an error is raised.
+"""
+
 
 def add_integer(a, b=98):
+    """
+    Adds two integers (or floats casted to int) and returns the result.
+    a and b must be integers or floats, otherwise raise a TypeError.
+
+    >>> add_integer(1, 2)
+    3
+    """
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
