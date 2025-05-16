ce9249ebf5376715c89236cfa0eded38f497a8ce
Helvlaska <ccastan@live.fr>
2025-05-16 16:27:33 +0200

    :hammer: (0-add_integer.py) : Refactor type checks in add_integer function for consistency and clarity

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,9 +13,9 @@ def add_integer(a, b=98):
     >>> add_integer(1, 2)
     3
     """
-    if type(a) not in [int, float]:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     a = int(a)
     b = int(b)
