a19b04ca9628919dcffd9f12c67ccbd7532e45f9
Helvlaska <ccastan@live.fr>
2025-05-16 21:55:11 +0200

    :hammer: (2-matrix_divided.py) : Refactor docstring and error messages in matrix_divided function for improved clarity and organization

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,17 +6,28 @@ by a given number. All results are rounded to 2 decimal places as float.
 
 
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix by div and returns floats rounded to 2 decimals."""
+    """
+    Divides all elements of a matrix by div and returns
+    floats rounded to 2 decimals.
+    """
 
     if not isinstance(matrix, list) or matrix == []:
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     if not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
-    if not all(isinstance(el, (int, float)) for row in matrix for el in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
+    for row in matrix:
+        for el in row:
+            if not isinstance(el, (int, float)) or isinstance(el, bool):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists)"
+                    " of integers/floats"
+                )
     row_len = len(matrix[0])
     if not all(len(row) == row_len for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
