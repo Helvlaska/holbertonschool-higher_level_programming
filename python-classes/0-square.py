2edc3145628691cdbee92ceac73f5715dde047e2
Helvlaska <ccastan@live.fr>
2025-05-19 11:51:36 +0200

    :hammer: (0-square.py) : Add docstring to Square class for improved documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Module contenant la classe Square
+pour représenter un carré géométrique.
+"""
+
 
 class Square:
+    """
+    Creation d'une class Square vide
+    """
+
     pass
