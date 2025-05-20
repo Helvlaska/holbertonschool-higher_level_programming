2513e173bda54f1a61a66f39e9396de4bda3000c
Helvlaska <ccastan@live.fr>
2025-05-20 14:57:48 +0200

    :sparkles: (0-rectangle.py) : Add Rectangle class with module documentation and empty class definition

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module contenant la classe Rectangle
+pour représenter un Rectangle géométrique.
+"""
+
+
+class Rectangle:
+    """
+    Creation d'une class Rectangle vide
+    """
+
+    pass
