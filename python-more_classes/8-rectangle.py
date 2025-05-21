284fa0f83f904bfdb674426328ec4b35cfde13eb
Helvlaska <ccastan@live.fr>
2025-05-21 14:18:54 +0200

    :hammer: (8-rectangle.py) : Add docstring

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -116,6 +116,9 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """
+        Retourne le plus grand rectangle (ou rect_1 s'ils sont égaux)
+        """
         if not isinstance(rect_1, Rectangle):
             raise TypeError(f"{rect_1} must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
