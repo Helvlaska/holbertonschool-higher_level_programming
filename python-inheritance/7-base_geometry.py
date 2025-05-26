dac7bd430eb16e07073f31c8b9b4efa621977d72
Helvlaska <ccastan@live.fr>
2025-05-26 17:51:23 +0200

    :hammer: (7-base_geometry.py) : Correction integer type check in integer_validator method

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -32,7 +32,7 @@ class BaseGeometry():
             ValueError: Si `value` est inférieur ou égal à 0.
         """
         self.name = name
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError(f"{self.name} must be an integer")
         elif value <= 0:
             raise ValueError(f"{self.name} must be greater than 0")
