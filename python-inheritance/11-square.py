ed4f3de1a8ff6d5c73939d9a624b4cfb944b39c5
Helvlaska <ccastan@live.fr>
2025-05-28 10:47:42 +0200

    :sparkles: (11-square.py) : Add Square class inheriting from Rectangle

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Création d'une classe qui hérite de la classe Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Classe représentant un carré, héritée de Rectangle.
+
+    Un carré est un rectangle avec des côtés égaux.
+    """
+    def __init__(self, size):
+        """
+        Initialise un carré avec une taille donnée.
+
+        Valide que la taille est un entier strictement positif,
+        puis initialise le rectangle avec largeur = hauteur = size.
+
+        Args:
+            size (int): La taille des côtés du carré.
+
+        Raises:
+            TypeError: Si size n'est pas un entier.
+            ValueError: Si size est inférieur ou égal à 0.
+        """
+        self.integer_validator("size", size)
+
+        super().__init__(size, size)
+
+        self.__size = size
+
+    def __str__(self):
+        """
+        Retourne une représentation textuelle du carré.
+
+        Format retourné :
+            [carré] <largeur>/<hauteur>
+
+        Returns:
+            str: Représentation formatée du carré.
+        """
+        return (f"[Square] {self.__size}/{self.__size}")
