c01c84d4e687ab993bc943590542ae0204bbfd10
Helvlaska <ccastan@live.fr>
2025-06-25 17:24:22 +0200

    :sparkles: Ajouter la création de la table second_table avec des enregistrements prédéfinis

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- Crée la table second_table avec des enregistrements prédéfinis
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(100), score INT);
+INSERT INTO second_table VALUES (1, 'John', 10);
+INSERT INTO second_table VALUES (2, 'Alex', 3);
+INSERT INTO second_table VALUES (3, 'Bob', 14);
+INSERT INTO second_table VALUES (4, 'George', 8);
