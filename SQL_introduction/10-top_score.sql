2c87088667baa25bce927b01749b05d73781a3d2
Helvlaska <ccastan@live.fr>
2025-06-25 18:24:34 +0200

    :hammer: Corriger la requête pour sélectionner uniquement les colonnes score et name dans second_table

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Liste tous les enregistrements de second_table triés par score décroissant
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT score, name FROM second_table ORDER BY score DESC;
