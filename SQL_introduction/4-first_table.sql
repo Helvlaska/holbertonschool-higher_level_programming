9b763b13302d8f64da90f5de3d5f545602c927e0
Helvlaska <ccastan@live.fr>
2025-06-25 17:02:39 +0200

    :hammer: (4-first_table.sql, 6-list_values.sql) : Ajouter un point-virgule manquant à la fin des instructions CREATE TABLE et SELECT

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- Ecrire une script qui crée une table dans la BDD seulement si elle n'existe
 -- pas
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
 -- Ecrire un script qui liste toutes les lignes d'une table de BDD
-SELECT * FROM first_table
+SELECT * FROM first_table;
