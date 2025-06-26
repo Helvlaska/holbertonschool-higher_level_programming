aed68ab02ace47afead624984c9e041f5a81241f
Helvlaska <ccastan@live.fr>
2025-06-26 20:10:50 +0200

    :sparkles: (6-states.sql): Ajouter un script pour créer la BDD hbtn_0d_usa et la table states avec les colonnes id et name

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Créer la BDD hbtn_0d_usa, vérifie si elle existe déjà
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
+-- Créer une table states, vérifie si elle existe déjà
+CREATE TABLE IF NOT EXISTS states (
+    -- l'id est un integer, auto généré, ne peu pas être null, et est une clé primaire
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    -- name ne peu pas être null, est une string, de max 256 caractères
+    name VARCHAR(256) NOT NULL
+);
