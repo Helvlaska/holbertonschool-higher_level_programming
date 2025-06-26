f09621da21e1a327a052e98f82d6fce0dce8925b
Helvlaska <ccastan@live.fr>
2025-06-26 13:48:19 +0200

    :hammer: (0-privileges.sql): Supprime la requête pour lister les utilisateurs MySQL dans le script de répertoriage des privilèges

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
 -- Ecrire un script qui repertorie tous les privilèges des users MySQL
--- Liste les utilisateurs MySQL
-SELECT user, host FROM mysql.user;
 -- Affiche les privilèges de user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 -- Affiche les privilèges de user_0d_2
