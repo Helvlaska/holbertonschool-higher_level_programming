98587c697523b496a4572e2128134974be4f7bff
Helvlaska <ccastan@live.fr>
2025-06-26 15:36:34 +0200

    :hammer: (2-create_read_user.sql): Corrige la syntaxe de la commande GRANT pour accorder le privilège SELECT à l'utilisateur user_0d_2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Crée l'utilisateur s'il n'existe pas déjà
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Accorde le privilège SELECT à l'utilisateur sur la BDD hbtn_0d_2
-GRANT SELECT PRIVILEGES ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
