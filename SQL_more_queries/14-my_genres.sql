09497bf8e2f7666a930ff28f1a6a7d24123452f6
Helvlaska <ccastan@live.fr>
2025-06-27 17:17:48 +0200

    :hammer: (14-my_genres.sql): Réorganiser et clarifier les étapes dans le script de sélection des genres de la série 'Dexter'

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,12 @@
--- Etape 4 - sélectionner ce qu'ont veux afficher
+-- Étape 1 - Sélectionner ce qu'on veut afficher (le nom du genre)
 SELECT tv_genres.name
--- Etape 1 - ma table principale
+-- Étape 2 - Définir la table principale
 FROM tv_genres
--- Etape 3 - filtrer ce que l'ont veut
-WHERE tv_shows.title = 'Dexter'
--- Etape 2.1 - associer la table principale à la secondaire
+-- Étape 3 - Associer la table principale à la table de liaison (tv_show_genres)
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
--- Etape 2.2 - associer la table secondaire à la table tertiaire
-JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
--- Etape 5 - Dans quel ordre ?
+-- Étape 4 - Associer la table de liaison à la table des shows
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+-- Étape 5 - Filtrer uniquement sur les shows avec le titre 'Dexter'
+WHERE tv_shows.title = 'Dexter'
+-- Étape 6 - Trier les résultats par ordre alphabétique
 ORDER BY tv_genres.name;
