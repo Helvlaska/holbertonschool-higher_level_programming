8b4c1c825883a998c6a23030076f6be82d77ef52
Helvlaska <ccastan@live.fr>
2025-06-28 19:46:33 +0200

    :sparkles: (3-my_safe_filter_states.py): Ajouter un script pour se connecter à une base de données MySQL et afficher les états correspondant à un nom donné

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Script qui se connecte à une base de données MySQL et affiche
+toutes les entrées de la table `states`dont le nom correspond à l'argument."""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """
+    Point d'entrée principal du script.
+    Utilise les arguments passés en ligne de commande :
+    - argv[1] : nom d'utilisateur MySQL
+    - argv[2] : mot de passe MySQL
+    - argv[3] : nom de la base de données
+    - argv[4] : nom du pays recherché dans la BDD
+    """
+    # Connexion à la BDD
+    connexion = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+
+    # Création d'un curseur pour lire et écrire dans la BDD
+    curseur = connexion.cursor()
+
+    # Stocker l'argument du pays recherché dans une variable
+    pays = sys.argv[4]
+
+    # Requête pour récupérer tout les states de la BDD
+    # correspondant à l'argument
+    curseur.execute("""
+        SELECT * FROM states
+        WHERE BINARY name = %s
+        ORDER BY id
+    """, (pays,))
+
+    # Récupération de la requête
+    lignes = curseur.fetchall()
+    for ligne in lignes:
+        print(ligne)
+
+    # Fermeture propre du curseur et de la connexion
+    curseur.close()
+    connexion.close()
