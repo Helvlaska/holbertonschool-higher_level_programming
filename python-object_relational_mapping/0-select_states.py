792c9c0ef95f5d640fbf71c50f6e7d4b8ead565d
Helvlaska <ccastan@live.fr>
2025-06-28 15:52:20 +0200

    :hammer: (0-select_states.py): Réorganiser le script pour utiliser les arguments de ligne de commande pour la connexion à la base de données

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
-import os
-from dotenv import load_dotenv
-import MySQLdb
+"""Script qui se connecte à une base de données MySQL et affiche
+toutes les entrées de la table `states`."""
+import MySQLdb, sys
 
-# Charger les variables du fichier .env pour mes identifiants
-load_dotenv()
 
-# Connexion à la BDD
-connexion = MySQLdb.connect(
-    host = "localhost",
-    port = 3306,
-    user=os.getenv("DB_USER"),
-    passwd=os.getenv("DB_PASSWORD"),
-    db = "hbtn_0e_0_usa"
-)
+if __name__ == "__main__":
+    """
+    Point d'entrée principal du script.
+    Utilise les arguments passés en ligne de commande :
+    - argv[1] : nom d'utilisateur MySQL
+    - argv[2] : mot de passe MySQL
+    - argv[3] : nom de la base de données
+    """
+    # Connexion à la BDD
+    connexion = MySQLdb.connect(
+        host = "localhost",
+        port = 3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
 
-# Création d'un curseur pour lire et écrire dans la BDD
-curseur = connexion.cursor()
+    # Création d'un curseur pour lire et écrire dans la BDD
+    curseur = connexion.cursor()
 
-# Requête pour récupérer tout les states de la BDD
-curseur.execute("SELECT * FROM states")
+    # Requête pour récupérer tout les states de la BDD
+    curseur.execute("SELECT * FROM states")
 
-# Récupération de la requête
-lignes = curseur.fetchall()
-for ligne in lignes:
-    print(ligne)
+    # Récupération de la requête
+    lignes = curseur.fetchall()
+    for ligne in lignes:
+        print(ligne)
 
-# Fermeture propre du curseur et de la connexion
-curseur.close()
-connexion.close()
+    # Fermeture propre du curseur et de la connexion
+    curseur.close()
+    connexion.close()
