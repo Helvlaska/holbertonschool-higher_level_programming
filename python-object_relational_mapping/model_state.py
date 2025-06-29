cf0b818fa4edd0c69ebd74400fa843e9878cecb6
Helvlaska <ccastan@live.fr>
2025-06-29 13:47:48 +0200

    :sparkles: (model_state.py): Ajouter la classe State pour représenter les états dans la base de données

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__= 'states'
+
+    id = Column(Integer(11), autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
