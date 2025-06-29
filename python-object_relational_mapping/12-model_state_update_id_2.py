7b27666ad783152acd6961dcea97c37ca8b17b0c
Helvlaska <ccastan@live.fr>
2025-06-29 22:59:46 +0200

    ✨ (12-model_state_update_id_2.py): Ajouter un script pour modifier le nom d'un objet State dans la BDD

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Script qui change le nom d'un objet State de la BDD."""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # Création de la connexion
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    # Création de toutes les tables si elles n'existent pas
+    # (id, name de la classe State)
+    Base.metadata.create_all(engine)
+
+    # Création d'une session (pour lire et écrire dans une BDD)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Modification du nom de l'objet
+    state = (
+        session.query(State)
+        .get(2)
+    )
+    state.name = 'New Mexico'
+    session.commit()
+
+    # Fermeture propre de la session
+    session.close()
