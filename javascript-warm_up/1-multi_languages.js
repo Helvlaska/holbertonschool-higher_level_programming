d5ad2cb9072712cfa74ae128c269cedf24c9f6fa
Helvlaska <ccastan@live.fr>
2025-07-11 22:06:40 +0200

    :hammer: (1-multi_languages.js): Exercice JavaScript

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -3,4 +3,4 @@ const c_langue = 'C is fun';
 const python_langue = 'Python is cool';
 const js_langue = 'JavaScript is amazing';
 
-console.log(c_langue + '\n' + python_langue + '\n' + js_langue);
+console.log(`${c_langue}\n${python_langue}\n${js_langue}`);
