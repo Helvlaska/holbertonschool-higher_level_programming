d063bc02f0ffb16b00318e9e859e47626690ba4c
Helvlaska <ccastan@live.fr>
2025-07-11 22:41:44 +0200

    :hammer: (5-to_integer.js): Exercice JavaScript

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-
 const arg = process.argv[2];
-// Transforme une value en entier
 const number = parseInt(arg);
 
-// isNaN = is Not a Number
 if (isNaN(number)) {
   console.log('Not a number');
 } else {
