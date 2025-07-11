83c2f3aa828262b542194b393c5ac4989ad68f4b
Helvlaska <ccastan@live.fr>
2025-07-11 22:02:23 +0200

    :sparkles: (1-multi_languages.js): Exercice JavaScript

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const c_langue = 'C is fun';
+const python_langue = 'Python is cool';
+const js_langue = 'JavaScript is amazing';
+
+console.log(c_langue);
+console.log(python_langue);
+console.log(js_langue);
